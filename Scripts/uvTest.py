a7d5ffcd7390637e3138e2a9ba73a30ba4052477
kamenoseiji <seiji.kameno@nao.ac.jp>
2023-11-10 16:12:38 -0300

    modified:   README.md 	deleted:    Scripts/checkGainExec.py 	modified:   Scripts/uvTest.py

--- Scripts/uvTest.py
+++ Scripts/uvTest.py
@@ -1,15 +1,16 @@
+#--------
+# This is a test code to check the sing convension of X and Y axes in uvmodelfit() in CASA
+#--------
 prefix = 'uid___A002_X109d26e_X12c34'
 BPCAL = 'J0423-0120'
 IQUV = [2.104, 0.0, 0.0, 0.0]
 #-------- Split channel-averaged data
-'''
 split(prefix + '.ms', spw='18', field='0', scan='3', outputvis='J0423-0120.chav.ms', datacolumn='corrected')
-'''
 msfile='J0423-0120.chav.ms'
 REFANT = 'DA64'
-'''
 plotms(msfile, xaxis='time', yaxis='amp', antenna='*&', spw='0', iteraxis='baseline', coloraxis='corr', gridrows=4, gridcols=4)
 setjy(vis=msfile, field=BPCAL, spw='', standard='manual', fluxdensity=IQUV, spix = [-0.7,0], reffreq = '343.4GHz', usescratch=False)
+#-------- Self calibration
 os.system('rm -rf TG0*')
 gaincal(vis=msfile, caltable = 'TG0', spw ='0', field='0', minsnr=5.0, solint='int', selectdata=True, solnorm=False, refant = REFANT, refantmode='strict', calmode = 'ap', gaintype='G')
 plotms('TG0', xaxis='time', yaxis='phase', spw='*', iteraxis='antenna')
@@ -17,12 +18,13 @@ applycal(vis=msfile, flagbackup=False, spw='0', interp='nearest', gaintable='TG0
 plotms(msfile, xaxis='time', yaxis='amp', ydatacolumn='corrected', antenna='*&', spw='0', iteraxis='baseline', coloraxis='corr', gridrows=4, gridcols=4)
 os.system('rm -rf ' + BPCAL + '.testclean*')
 tclean(vis=msfile,  imagename=BPCAL + '.testclean', field=BPCAL, cell=['0.001arcsec'], imsize=[512,512], spw='0', deconvolver='hogbom', weighting='natural', interactive=True, niter=1000, mosweight=True, pbcor=False)
-                                                         
+#-------- Position shift
 phaseshift(msfile, outputvis=BPCAL + '.shift.ms', datacolumn='corrected', phasecenter='J2000 04:23:15.797 -01.20.33.00')
 plotms(BPCAL + '.shift.ms', xaxis='time', yaxis='phase', ydatacolumn='corrected', antenna='*&', spw='0', iteraxis='baseline', coloraxis='corr', gridrows=4, gridcols=4)
 os.system('rm -rf ' + BPCAL + '.shiftclean*')
+#-------- Check position shift in the image
 tclean(vis=BPCAL + '.shift.ms',  imagename=BPCAL + '.shiftclean', field=BPCAL, cell=['0.001arcsec'], imsize=[512,512], spw='0', deconvolver='hogbom', weighting='natural', interactive=True, niter=1000, mosweight=True, pbcor=False)
-'''
+#-------- uvmodelfit for the position-shift image
 os.system('rm -rf cl.test*')
 uvmodelfit(vis= BPCAL + '.shift.ms', spw='0', sourcepar=[2.20, 0.071, -0.060], varypar=[True, True, True], outfile='cl.test')
 
